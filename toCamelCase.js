8fee34bf18d29d5358fc3e0a26c6bdeefffaab24
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-10-06 14:17:08 -0400

    slight update

--- toCamelCase.js
+++ toCamelCase.js
@@ -1,7 +1,5 @@
 // Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
 
-
-
 //toCamelCase("the-stealth-warrior"); // returns "theStealthWarrior"
 //toCamelCase("The_Stealth_Warrior"); // returns "TheStealthWarrior"
 
