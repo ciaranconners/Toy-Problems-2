882b23e4ca79e7ddf076bf7153bab12735a2ec66
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-10-18 15:17:02 -0400

    adds an implementation of a BST inorder traversal method

---
+++ inorderTraversal.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const inorderTraversal = (root) => {
+    let result = [];
+    if (root.left) {
+        result = result.concat(inorderTraversal(root.left));
+    }
+    result.push(root.val);
+    if (root.right) {
+        result = result.concat(inorderTraversal(root.right));
+    }
+    return result;
+};
