c405aa92e38e79e94ee1b1eabbb4cd63e9a5c351
Ciaran Martin Conners <cmc2295@columbia.edu>
2017-10-24 22:29:16 -0400

    Update printLinkedList.js

--- printLinkedList.js
+++ printLinkedList.js
@@ -1,25 +1,21 @@
 function print(head) {
-    function traverse(head) {
-      let result = [];
-      if (head) {
-        result.push(head.value);
-        if (head.next) {
-          result = result.concat(traverse(head.next));
-        }
-      }
-      return result;
-    }
-    let values = traverse(head);
-    console.log(values);
-    for (let i = 0; i < values.length; i++) {
-      let current = values[i];
-      if (i === values.length - 1) {
-        console.log(current);
-      } else {
-        console.log(current + '\n');
+  function traverse(head) {
+    let result = [];
+    if (head) {
+      result.push(head.value);
+      if (head.next) {
+        result = result.concat(traverse(head.next));
       }
     }
+    return result;
+  }
+  let values = traverse(head);
+  for (let i = 0; i < values.length; i++) {
+    let current = values[i];
+    if (i === values.length - 1) {
+      console.log(current);
+    } else {
+      console.log(current + '\n');
+    }
+  }
 }
-
-
-
