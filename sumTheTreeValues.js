cb6b1877b58472e570fbd0be034e817f25e5bd0a
Ciaran Martin Conners <ciaranconners@gmail.com>
2017-10-24 22:23:39 -0400

    deletes extra copy of addTwoNumbers; adds a codewars tree traversal kata

---
+++ sumTheTreeValues.js
@@ -0,0 +1,15 @@
+// a simple method to sum the values contained in a binary tree
+
+function sumTheTreeValues(root){
+  let toSum = [];
+  if (root) {
+    toSum.push(root.value);
+  }
+  if (root.left) {
+    toSum = toSum.concat(sumTheTreeValues(root.left));
+  }
+  if (root.right) {
+    toSum = toSum.concat(sumTheTreeValues(root.right));
+  }
+  return toSum.reduce((a, v) => a + v);
+}
